af71e6df11af7a044b97d812c7ee15ef02e7f518
rushikeshgadekar <rushikeshgadekar17399@gmail.com>
2021-04-08 03:06:06 +0530

    added normal section

--- pages/scroll.js
+++ pages/scroll.js
@@ -43,3 +43,16 @@ $(document).ready(function () {
     }
   });
 });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#myBtn").fadeIn();
+    } else {
+      $("#myBtn").fadeOut();
+    }
+  });
+  $("#myBtn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
